b1aeb6a325a0d794763a21e50c6313f9f1a661d4
nupur-0123 <105721546+nupur-0123@users.noreply.github.com>
2022-05-18 08:30:51 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5600;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`app started on port ${port}`);
+})
